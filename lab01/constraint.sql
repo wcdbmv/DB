3cd50a98c036a7a81bd2cab9b6252c5f8444731e
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-11-01 17:29:03 +0300

    Rename schema

--- lab01/constraint.sql
+++ lab01/constraint.sql
@@ -1,8 +1,8 @@
-alter table fs_db.users
+alter table fs.users
 	add constraint name_not_empty check (name != '');
 
-alter table fs_db.groups
+alter table fs.groups
 	add constraint name_not_empty check (name != '');
 
-alter table fs_db.inodes
+alter table fs.inodes
 	add constraint name_not_empty check (name != '');
--- lab01/create.sql
+++ lab01/create.sql
@@ -1,40 +1,40 @@
-create schema if not exists fs_db;
+create schema if not exists fs;
 
-drop table if exists fs_db.inodes cascade;
-drop table if exists fs_db.types cascade;
-drop table if exists fs_db.users cascade;
-drop table if exists fs_db.groups cascade;
-drop table if exists fs_db.users_groups cascade;
+drop table if exists fs.inodes cascade;
+drop table if exists fs.types cascade;
+drop table if exists fs.users cascade;
+drop table if exists fs.groups cascade;
+drop table if exists fs.users_groups cascade;
 
 -- вообще-то у пацанов табы шириной 8
 
-create table fs_db.types (
+create table fs.types (
 	id		serial primary key,
 	name		varchar(16) not null,
 	shortcut	char not null
 );
 
-create table fs_db.users (
+create table fs.users (
 	id		serial primary key,
 	name		varchar(32) not null
 );
 
-create table fs_db.groups (
+create table fs.groups (
 	id		serial primary key,
 	name		varchar(32) not null
 );
 
-create table fs_db.users_groups (
+create table fs.users_groups (
 	id		serial primary key,
-	user_id		serial not null references fs_db.users(id),
-	group_id	serial not null references fs_db.groups(id)
+	user_id		serial not null references fs.users(id),
+	group_id	serial not null references fs.groups(id)
 );
 
-create table fs_db.inodes (
+create table fs.inodes (
 	inode		serial primary key,
 	name		varchar(64) not null,
-	parent_inode	integer references fs_db.inodes(inode),
-	type_id		serial not null references fs_db.types(id),
-	owner_id	serial not null references fs_db.users(id),
-	group_id	serial not null references fs_db.groups(id)
+	parent_inode	integer references fs.inodes(inode),
+	type_id		serial not null references fs.types(id),
+	owner_id	serial not null references fs.users(id),
+	group_id	serial not null references fs.groups(id)
 );
